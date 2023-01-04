751f72e4cc56b6c118206bd1713daa0fae550512
Fraz Shabbir <fraz.shabbir54@gmail.com>
2023-01-05 00:23:38 +0500

    Update _navbar.blade.php

--- resources/views/frontend/partials/_navbar.blade.php
+++ resources/views/frontend/partials/_navbar.blade.php
@@ -123,7 +123,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="category_page.html">All</a>
+                            <a class="nav-link" href="{{route('public.products')}}">All</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Today's Deals</a>
